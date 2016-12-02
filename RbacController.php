9e03cba854d88358eea919638d80ab9584cd0080
carno <spamer2000@list.ru>
2016-12-02 14:02:28 +0500

    prepare advanced

--- RbacController.php
+++ RbacController.php
@@ -4,22 +4,33 @@ namespace carono\yii2rbac;
 
 use yii\base\InlineAction;
 use yii\console\Controller;
+use yii\helpers\ArrayHelper;
 use yii\helpers\Console;
+use yii\helpers\StringHelper;
 
 class RbacController extends Controller
 {
     public $userClass = 'app\models\User';
     public $roles = [];
     public $permissions = [];
-    public $applicationId;
+    public $basicId;
+    public $frontendId;
+    public $backendId;
 
     public function init()
     {
         RoleManager::$userClass = $this->userClass;
-        RoleManager::$defaultApplicationId = $this->applicationId;
-        if (!$this->applicationId) {
-            Console::output('Attribute "applicationId" is required');
-            exit;
+        if (self::isAdvanced()) {
+            if (!$this->backendId) {
+                $this->backendId = ArrayHelper::getValue(self::getBackendConfig(), 'id');
+            }
+            if (!$this->frontendId) {
+                $this->frontendId = ArrayHelper::getValue(self::getFrontendConfig(), 'id');
+            }
+        } else {
+            if (!$this->basicId) {
+                $this->basicId = ArrayHelper::getValue(self::getBasicConfig(), 'id');
+            }
         }
         parent::init();
     }
@@ -89,6 +100,20 @@ class RbacController extends Controller
         $transaction->commit();
     }
 
+    public function getApplicationIdByControllerClass($controller)
+    {
+        if (StringHelper::startsWith(get_class($controller), 'frontend')) {
+            $id = $this->frontendId;
+        } elseif (StringHelper::startsWith(get_class($controller), 'backend')) {
+            $id = $this->backendId;
+        } elseif (StringHelper::startsWith(get_class($controller), 'app')) {
+            $id = $this->basicId;
+        } else {
+            $id = null;
+        }
+        return $id;
+    }
+
     public function normalizePermission($expressionPermission)
     {
         if (strpos($expressionPermission, '*') !== false) {
@@ -97,7 +122,10 @@ class RbacController extends Controller
             $controllers = $this->collectControllers($modules, $arr[1]);
             $actions = $this->collectActions($controllers, $arr[2]);
             $permissions = [];
+
+
             foreach ($actions as $action) {
+                RoleManager::$defaultApplicationId = self::getApplicationIdByControllerClass($action->controller);
                 $permissions[] = RoleManager::formPermissionByAction($action);
             }
             return $permissions;
@@ -129,36 +157,85 @@ class RbacController extends Controller
         return $actions;
     }
 
+    public static function getBackendConfig()
+    {
+        return ArrayHelper::merge(
+            require(\Yii::getAlias('@common/config/main.php')),
+            require(\Yii::getAlias('@common/config/main-local.php')),
+            require(\Yii::getAlias('@backend/config/main.php')),
+            require(\Yii::getAlias('@backend/config/main-local.php'))
+        );
+    }
+
+    public static function getFrontendConfig()
+    {
+        return ArrayHelper::merge(
+            require(\Yii::getAlias('@common/config/main.php')),
+            require(\Yii::getAlias('@common/config/main-local.php')),
+            require(\Yii::getAlias('@frontend/config/main.php')),
+            require(\Yii::getAlias('@frontend/config/main-local.php'))
+        );
+    }
+
+    public static function getBasicConfig()
+    {
+        return require(\Yii::getAlias('@app/config/web.php'));
+    }
+
+    public static function isAdvanced()
+    {
+        return key_exists('@backend', \Yii::$aliases);
+    }
+
     public function collectControllers($modules, $id)
     {
         $controllers = [];
         foreach ($modules as $module) {
             if ($id == "*") {
                 $f = function ($v) {
-                    return str_replace('Controller', '', basename($v, '.php'));
+                    return [str_replace('Controller', '', basename($v, '.php')) => $v];
                 };
                 if ($module) {
                     $names = array_map(
                         $f, glob(\Yii::getAlias("@app/modules/{$module->id}/controllers/*Controller.php"))
                     );
                 } else {
-                    $names = array_map($f, glob(\Yii::getAlias('@app/controllers/*Controller.php')));
+                    $backend = $frontend = $basic = [];
+                    if (self::isAdvanced()) {
+                        $backend = array_map($f, glob(\Yii::getAlias('@backend/controllers/*Controller.php')));
+                        $frontend = array_map($f, glob(\Yii::getAlias('@frontend/controllers/*Controller.php')));
+                    } else {
+                        $basic = array_map($f, glob(\Yii::getAlias('@app/controllers/*Controller.php')));
+                    }
+                    $names = array_merge($backend, $frontend, $basic);
                 }
             } else {
                 $names = [$id];
             }
-            foreach (array_filter($names) as $name) {
-                if ($module) {
-                    $className = join('\\', ['app', 'modules', $module->id, 'controllers', $name . "Controller"]);
-                } else {
-                    $className = join('\\', ['app', 'controllers', $name . "Controller"]);
-                }
+            foreach (array_filter($names) as $elem) {
+                $name = key($elem);
+                $file = current($elem);
+                $className = self::extractClassByPath($file);
                 if (class_exists($className)) {
                     $controllers[] = new $className($name, $module);
                 }
             }
         }
         return $controllers;
+    }
+
+    public static function extractClassByPath($file)
+    {
+        if (file_exists($file)) {
+            $content = file_get_contents($file);
+            $namespace = '';
+            $class = basename($file, '.php');
+            if (preg_match('/namespace\s+(.+);/i', $content, $m)) {
+                $namespace = $m[1];
+            }
+            return $namespace . '\\' . $class;
+        }
+        return false;
     }
 
     public function collectModules($id = '*')

6355aa8c7fa4ec99244ffb05d7dfcdaf2391846a
carono <info@carono.ru>
2017-10-10 15:27:30 +0500

    replace AuthorBehavior

--- AuthorBehavior.php
+++ AuthorBehavior.php
@@ -1,46 +1,6 @@
 <?php
 namespace carono\yii2rbac;
 
-use yii\behaviors\AttributeBehavior;
-use yii\db\BaseActiveRecord;
-use yii\helpers\ArrayHelper;
-
-class AuthorBehavior extends AttributeBehavior
+class AuthorBehavior extends \carono\yii2behaviors\AuthorBehavior
 {
-	public $attributes;
-	public $asRobot = true;
-	public $robot = null;
-	public $createdAtAttribute = 'creator_id';
-	public $updatedAtAttribute = 'updater_id';
-
-	public function init()
-	{
-		parent::init();
-
-		if (empty($this->attributes)) {
-			$this->attributes = [
-				BaseActiveRecord::EVENT_BEFORE_INSERT => [$this->createdAtAttribute, $this->updatedAtAttribute],
-				BaseActiveRecord::EVENT_BEFORE_UPDATE => $this->updatedAtAttribute,
-			];
-		}
-	}
-
-	/**
-	 * @inheritdoc
-	 */
-	protected function getValue($event)
-	{
-		if ($event->name == BaseActiveRecord::EVENT_BEFORE_INSERT) {
-			$attr = ArrayHelper::getValue(
-				$this->attributes, BaseActiveRecord::EVENT_BEFORE_INSERT, $this->createdAtAttribute
-			);
-			if (is_array($attr)) {
-				$attr = current($attr);
-			}
-			if ($value = $event->sender->{$attr}) {
-				return $value;
-			}
-		}
-		return CurrentUser::getId($this->asRobot, $this->robot);
-	}
 }
